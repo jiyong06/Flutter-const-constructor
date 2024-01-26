66704331747d754c85de84a9dbf05c50aeeafc26
박지용 <gdragon0228@naver.com>
2024-01-26 15:09:03 +0900

    stydy day3

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_const_constructor/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
